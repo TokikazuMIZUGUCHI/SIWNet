11cef43a1d029b54ce5f338bcf696739426f4cef
TokikazuMIZUGUCHI <mizuguchi-tokikazu@g.ecc.u-tokyo.ac.jp>
2024-11-10 23:42:57 +0900

    a

--- friction_dataset.py
+++ friction_dataset.py
@@ -1,34 +1,64 @@
-from torch.utils.data.dataset import Dataset
+import os
 import pandas as pd
 import numpy as np
 from PIL import Image
 import torch
-
+from torch.utils.data import Dataset
 
 class FrictionDataset(Dataset):
-    # implementation of the dataset used in the study.
+    """
+    Implementation of the dataset used in the study.
+    Loads image paths and grip factors from CSV files and prepares them for use in training.
+    """
     def __init__(self, data_paths, transform):
-        # load data from csv-files
-        data_list = [pd.read_csv(path, header=None) for path in data_paths]
-        self.data = pd.concat(data_list)
+        """
+        Initializes the dataset by loading data from the provided CSV files.
+
+        :param data_paths: List of paths to CSV files containing the data.
+        :param transform: PyTorch transforms to apply to the images.
+        """
+        # Load data from CSV files
+        data_list = [pd.read_csv(path) for path in data_paths]  
+        self.data = pd.concat(data_list).reset_index(drop=True)  # Reset index to ensure continuous indexing
         self.data_len = len(self.data.index)
         self.img_path = np.asarray(self.data.iloc[:, 0])
-        self.grip = np.asarray(self.data.iloc[:, 1])
-        # scale grip factor to friction factor (0...1) for normalisation
+
+        # Convert grip factor from string to float
+        self.grip = self.data.iloc[:, 1].astype(float)
+        
+        # Scale grip factor to friction factor (0...1) for normalisation
         self.friction_factor = (self.grip - 0.09) / (0.82 - 0.09)
  
         self.transform = transform
     
     def __getitem__(self, index):
-        img_path = self.img_path[index]
-        img = Image.open(img_path).convert("RGB")
-        # get friction factor
-        ff = self.friction_factor[index]
-        ff = np.float32(ff)
-        # transform image 
-        img = self.transform(img)
-        
-        return (img, ff)
+        """
+        Retrieves an item from the dataset at the specified index.
+
+        :param index: Index of the item to retrieve.
+        :return: Tuple containing the transformed image and its corresponding friction factor.
+        """
+        try:
+            img_path = self.img_path[index]
+            img = Image.open(img_path).convert("RGB")
+            # Get friction factor
+            ff = self.friction_factor[index]
+            ff = np.float32(ff)
+            # Transform image 
+            img = self.transform(img)
+            
+            return (img, ff)
+        except KeyError as e:
+            print(f"Index {index} is out of bounds. Error: {e}")
+            raise
+        except Exception as e:
+            print(f"An error occurred while processing index {index}: {e}")
+            raise
 
     def __len__(self):
+        """
+        Returns the total number of items in the dataset.
+
+        :return: Length of the dataset.
+        """
         return self.data_len
